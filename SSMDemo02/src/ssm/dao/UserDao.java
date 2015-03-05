1d0904477ac7203a61a84a72c9d038353560ae8a
errormessage <990193251@qq.com>
2015-03-05 08:47:40 +0800

    upload file SSMDemo02 for test

---
+++ SSMDemo02/src/ssm/dao/UserDao.java
@@ -0,0 +1,26 @@
+/*
+ * www.dyr.com
+ * Copyright (c) 2014 All Right Reserved.
+ *    版权					                   保留
+ */
+/**
+ * 
+ */
+package ssm.dao;
+
+/**
+ * Project: SSMDemo02
+ * Package: ssm.dao
+ * FileName: UserDao.java 
+ * Comments(意见):
+ * JDK Version:
+ * Author:谢洪章
+ * Create Date: 2015-3-4下午3:07:53
+ * Modified By(修改人):Casper
+ * Modified Time(修改时间):
+ * What is Modified:
+ * Version(版本):
+ */
+public interface UserDao {
+
+}
